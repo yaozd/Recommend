42a12200b582b672e7b4b2398f55f282e303cc9f
liujm7 <290671981@qq.com>
2017-10-10 15:15:15 +0800

    格式化主函数

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,31 +12,64 @@ package main;
 
 import org.ujmp.core.*;
 import org.ujmp.core.calculation.Calculation;
-import recommend.ItemBasedCF;
-import recommend.ModelCF;
-import recommend.SlopOne;
-import recommend.UserBasedCF;
+import recommend.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static common.Utils.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Matrix ratingsMatrix = readFileAsRatingsMatrix("D:\\work\\liujm\\2017\\9\\20170911\\ml-100k\\ml-100k\\u.data");
-        Matrix test = impulation(ratingsMatrix, "item");
-//////        Double sim = getSimilarity(test.selectColumns(Calculation.Ret.NEW, 0).transpose(), test.selectColumns(Calculation.Ret.NEW, 0).transpose(), "else");
-//////        System.out.println(sim);
-//        ItemBasedCF itemBasedCF = new ItemBasedCF(test, "cosine");
-//        SlopOne slopOne=new SlopOne(ratingsMatrix);
-        ModelCF modelCF = new ModelCF(test);
-//        Matrix predictionsMatrix = slopOne.CalcRatings();
-//        Matrix predictionsMatrix = modelCF.SGD(10,10,1.0,0.1,0.001);
-        Matrix predictionsMatrix = modelCF.SVD("userAverage");//.round(Calculation.Ret.NEW);
+    public static void printResult(Matrix predictionsMatrix) {
         System.out.println(predictionsMatrix.getRowCount());
         System.out.println(predictionsMatrix.getColumnCount());
         for (int i = 0; i < predictionsMatrix.getRowCount(); i++) {
             System.out.println(predictionsMatrix.selectRows(Calculation.Ret.NEW, i));
         }
+    }
 
+    public static void testUserBasedCF(Matrix ratingsMatrix) {
+        UserBasedCF userBasedCF = new UserBasedCF(ratingsMatrix, "cosine");
+        Matrix predictionsMatrix = userBasedCF.CalcRatings();
+        printResult(predictionsMatrix);
 
     }
+
+    public static void testItemBasedCF(Matrix ratingsMatrix) {
+        ItemBasedCF itemBasedCF = new ItemBasedCF(ratingsMatrix, "cosine");
+        Matrix predictionsMatrix = itemBasedCF.CalcRatings();
+        printResult(predictionsMatrix);
+    }
+
+    public static void testSlopOne(Matrix ratingsMatrix) {
+        SlopOne slopOne = new SlopOne(ratingsMatrix);
+        Matrix predictionsMatrix = slopOne.CalcRatings();
+        printResult(predictionsMatrix);
+    }
+
+    public static void testModeCF(Matrix ratingsMatrix) {
+        ModelCF modelCF = new ModelCF(ratingsMatrix);
+        Matrix predictionsMatrix = modelCF.SVD("userAverage");
+        printResult(predictionsMatrix);
+    }
+
+
+    public static void testCBF(Matrix ratingsMatrix, Matrix itemsFeatureMatrix) {
+        CBF cbf = new CBF(ratingsMatrix, itemsFeatureMatrix, "userAverage");
+        Matrix predictionsMatrix = cbf.CBFAverage();
+        printResult(predictionsMatrix);
+    }
+
+    public static void testHybridCbfCF(Matrix ratingsMatrix, Matrix itemsFeatureMatrix) {
+        HybridCbfCF hybridCbfCF = new HybridCbfCF(ratingsMatrix, itemsFeatureMatrix, "userAverage");
+        Matrix predictionsMatrix = hybridCbfCF.CalcRatings();
+        printResult(predictionsMatrix);
+    }
+
+    public static void main(String[] args) {
+        Matrix ratingsMatrix = readFileAsRatingsMatrix("D:\\work\\liujm\\2017\\9\\20170911\\ml-100k\\ml-100k\\u.data");
+        Matrix itemsFeatureMatrix = readFileAsItemsFeatureMatrix("D:\\work\\liujm\\2017\\9\\20170911\\ml-100k\\ml-100k\\u.item");
+        testHybridCbfCF(ratingsMatrix, itemsFeatureMatrix);
+    }
+
 }
